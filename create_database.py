e73ce91de0aec1e2f294944926a17d5786dae732
Roshan Jadhav <128218543+RoshanV2310@users.noreply.github.com>
2024-01-29 23:24:24 +0530

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 # MySQL connection parameters
 db_config = {
-    'host': os.environ.get('DB_HOST', 'localhost'),
-    'user': os.environ.get('DB_USERNAME', 'root'),
-    'password': os.environ.get('DB_PASSWORD', ''),
-    'database': 'mysql',
+    'host': os.environ.get('DB_HOST'),  # Align with your app.py setting
+    'user': os.environ.get('DB_USERNAME'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'database': os.environ.get('DB_NAME', 'mydb'),  # Align with your app.py setting
     'port': int(os.environ.get('DB_PORT', 3306)),
 }
 
